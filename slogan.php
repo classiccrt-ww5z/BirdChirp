1c518c2192bf13fd7636ccf37f2b688cf3c4db7d
This Fella Knows Python <classiccrt@gmail.com>
2026-07-03 20:24:04 -0700

    Update slogan.php

--- slogan.php
+++ slogan.php
@@ -1,19 +1,7 @@
 <?php
 $illshowyou = [
-    "I'll show you...",
-    "bitview-main.zip",
-    "WHO DELETED ADMIN PANEL",
-    "You're putting words in my mouth that i never said.",
-    "lol i remember femtanyl in 2012 very nostalgic",
-    "burrito",
-    "https://files.catbox.moe/thlxk2.rar",
-    "iPhone XS Max Rose Gold 64GB Unlocked",
-    "https://ryzenaiko.com/",
-    "What the fuck is a backend?",
-    "meow in the bag",
-    "and others are just realy weird like whiteguys fartinmyface",
-    "i will goon to the admin panel",
-    
+    "so you like put slogans here",
+"so yea",
 ];
 
 echo $illshowyou[array_rand($illshowyou)];
